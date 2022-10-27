e82a97835b41b9ca0ca164e60f3101860f7319ba
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-10-27 13:26:16 -0300

    setup inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import styled from "styled-components";
+import UserContext from "./contexts/Auth";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <UserContext.Provider value={"joao"}>
+    <Routes>
+      <Route path="/" element={<Login/>}/>
+    </Routes>
+    </UserContext.Provider>
+    </BrowserRouter>
+    
+
+    
   );
 }
 
